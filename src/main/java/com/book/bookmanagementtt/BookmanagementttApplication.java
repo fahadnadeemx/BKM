28998fdc797a952ea2f1347c74271af49f5a9b51
fahadnadeemx <85766937+fahadnadeemx@users.noreply.github.com>
2022-03-22 22:21:32 +0100

    first commit

---
+++ src/main/java/com/book/bookmanagementtt/BookmanagementttApplication.java
@@ -0,0 +1,13 @@
+package com.book.bookmanagementtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanagementttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmanagementttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/bookmanagementtt/BookmanagementttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.bookmanagementtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagementttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
